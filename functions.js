51b3bffa0cebbba273b4350154c631c1634637e8
mahume <michael.a.hume@gmail.com>
2019-03-03 20:53:40 -0700

    Code update 20:53

--- functions.js
+++ functions.js
@@ -1,15 +1,86 @@
+require("dotenv").config()
+// NPM Packages
+const axios = require('axios')
 const chalk = require('chalk')
+const Spotify = require('node-spotify-api')
+// Files
+const fs = require('fs')
+const keys = require('./keys')
+
+const spotify = new Spotify(keys.spotify);
 
 function displayCommands() {
-    console.log(chalk.underline.bold('Here are the available commands:'))
+    console.log(chalk.underline.bold('Available commands:'))
+    console.log(chalk.bold(`
+    Function            Command               Search Parameter`))
     console.log(`
-    Search concerts:    concert-this
-    Search songs:       spotify-this-song
-    Search movies:      movie-this
-    Search from file:   do-what-it-says
+    Search concerts:         concert-this       +  <artist/band name>
+    Search songs:            spotify-this-song  +  <song name>
+    Search movies:           movie-this         +  <movie title>
+    Search from text file:   do-what-it-says    +   
     `)
 }
 
+function commandError() {
+    console.log(chalk.red.bold(`Type "help" to see list of commands`))
+}
+
+function spotifySearch() {
+    console.log(`Results for the song: ${searchQuery}`)
+    spotify
+    .search({
+        type: 'track',
+        query: searchQuery,
+        limit: 1
+    })
+    .then(function(response) {
+        console.log(`
+        Artist:   ${response.tracks.items[0].album.artists[0].name}
+        Track:    ${response.tracks.items[0].name}
+        Preview:  ${response.tracks.items[0].preview_url}
+        Album:    ${response.tracks.items[0].album.name}
+        `);
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
+}
+
+function omdbSearch() {
+    console.log(`Results for the movie: ${searchQuery}`)
+    const queryURL = `http://www.omdbapi.com/?t=${searchQuery}&y=&plot=short&apikey=trilogy`
+    axios.get(queryURL)
+    .then(function(response) {
+        console.log(`
+        Title:                      ${response.data.Title}
+        Release Year:               ${response.data.Year}              
+        IMDB Rating:                ${response.data.imdbRating}
+        Rotten Tomatoes Rating:     ${response.data.Ratings[1].Value}
+        Country:                    ${response.data.Country}
+        Language:                   ${response.data.Language}
+        Actors:                     ${response.data.Actors}
+        Plot:                       
+        ${response.data.Plot}
+        `);
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
+}
+
+function doWhatItSays() {
+    const dataBuffer = fs.readFileSync('random.txt')
+    const dataJSON = dataBuffer.toString()
+    const dataArr = dataJSON.split(',')
+    command = dataArr[0]
+    searchQuery = dataArr[1]
+    spotifySearch()
+}
+
 module.exports = {
-    displayCommands
+    displayCommands,
+    commandError,
+    spotifySearch,
+    omdbSearch,
+    doWhatItSays
 }
--- liri.js
+++ liri.js
@@ -1,16 +1,10 @@
 require("dotenv").config()
-const keys = require("./keys.js")
-const axios = require('axios')
-const Spotify = require('node-spotify-api')
-const moment = require('moment')
+
+// Files
 const func = require('./functions')
 
-
-const spotify = new Spotify(keys.spotify);
-
-
 let command = process.argv[2]
-let searchQuery = process.argv.slice(3).join(" ")
+searchQuery = process.argv.slice(3).join(" ")
 
 switch (command) {
     case 'help':
@@ -20,42 +14,16 @@ switch (command) {
         console.log('Searching for concert')
         break;
     case 'spotify-this-song':
-        console.log('Searching for song')
-        spotifySearch();
+        func.spotifySearch()
         break;
     case 'movie-this':
-        console.log('Searching for movie')
+        func.omdbSearch()
         break;
     case 'do-what-it-says':
-        console.log('Do what it says')
+        func.doWhatItSays()
         break;
     default:
-        console.log(chalk.red.inverse.bold(`Type "help" to see list of commands`))
+        func.commandError()
         break;
 }
- 
-function spotifySearch() {
-    spotify
-    .search({
-        type: 'track',
-        query: searchQuery,
-        limit: 1
-    })
-    .then(function(response) {
-        // Song's name        
-        // Preview link 
-        const artist = response.tracks.items[0].album.artists[0].name
-        const track = response.tracks.items[0].name
-        const link = response.tracks.items[0].preview_url
-        const album = response.tracks.items[0].album.name
-        console.log(`
-        Artist: ${artist}
-        Track: ${track}
-        Preview: ${link}
-        Album: ${album}
-        `);
-    })
-    .catch(function(err) {
-        console.log(err)
-    })
-}
+
