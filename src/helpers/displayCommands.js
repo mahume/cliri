d7d59787f7f6e4c91b28f15a3f9d0d3eac46dfd0
mahume <michael.a.hume@gmail.com>
2019-07-07 18:09:11 -0600

    Fixing indentation

--- src/helpers/displayCommands.js
+++ src/helpers/displayCommands.js
@@ -14,17 +14,17 @@ const displayCommands = () => {
   const command = makeBoldRed('Command');
   const searchQuery = makeBoldBlue('Search Query');
   const searchSyntax = makeItalic('Search Syntax:');
-  const node = makeGreen('node');
+  const node = makeGreen('cliri');
   // Display
   console.log(`
-    ${searchFor}       ${command}                ${searchQuery}`);
+      ${searchFor}       ${command}                ${searchQuery}`);
   console.log(`
-    Concerts          concert            +   <artist/band name>
-    Songs             song               +   <song name>
-    Movies            movie              +   <movie title>
-    Text file         text`);
+      Concerts          concert            +   <artist/band name>
+      Songs             song               +   <song name>
+      Movies            movie              +   <movie title>
+      Text file         text`);
   console.log(`
-    ${searchSyntax}    ${node} cliri.js ${command} ${searchQuery}
+      ${searchSyntax}    ${node} ${command} ${searchQuery}
   `);
 };
 
